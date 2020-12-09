fd1e95a1cb0971d4bd96a017d0b74fd92dbf9627
Marcin Grzeszczak <marcin.grzeszczak+github@gmail.com>
2020-12-09 19:48:55 +0100

    Created header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HeaderComponent} from './Components/Header/Header.component'
 
-import {MatButtonModule} from '@angular/material/button'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
