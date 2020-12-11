38401d19ab1542b103d8e25481283685b5c26592
Marcin Grzeszczak <marcin.grzeszczak+github@gmail.com>
2020-12-11 15:37:40 +0100

    Loading data form store and showing them

--- src/app/Components/Details/Details.component.ts
+++ src/app/Components/Details/Details.component.ts
@@ -1,23 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PlanetsStoreService } from 'src/app/Services/PlanetsStore.service';
+import {Film,Resident, PlanetDetails} from '../../DataSchemes.model'
+
+
 
 @Component({
   selector: 'app-details',
   templateUrl: './Details.component.html',
   styleUrls: ['./Details.component.css']
 })
-export class DetailsComponent implements OnInit {
 
-  tiles = [
-    {title:'Residents', data:['aaa','bbb','ccc']},
-    {title:'Details', data:['aaa','bbb','ccc']},
-    {title:'Films', data:['aaa','bbb','ccc']}
-  ]
-  
-  constructor() { }
+export class DetailsComponent implements OnInit {
+  films: Film[] = [{title:"aaaaa"},{title:"aaaaa"},{title:"aaaaa"},{title:"aaaaa"},{title:"aaaaa"},{title:"aaaaa"},{title:"aaaaa"}]
+  residents: Resident[] = []
+  details: PlanetDetails = null
+  constructor(private route: ActivatedRoute ,private store: PlanetsStoreService) { }
 
 
 
   ngOnInit(): void {
+    const planetName =  this.route.snapshot.params['name']
+    this.store.getFilms(planetName).subscribe(
+        data => {
+          this.films = data
+        },
+        error => {
+          console.log(error)
+        })
+    
+    this.store.getResidents(planetName).subscribe(
+      data => {
+        this.residents = data
+      },
+      error => {
+        console.log(error)
+      }
+    )
+
+    this.store.getPlanetDetails(planetName).subscribe(planet => {
+       this.details = <PlanetDetails> planet
+       console.log(this.details)
+    }) 
   }
 
 }
