7ad3b0c9c7056a6037c00eab6432743308f02ab7
Marcin Grzeszczak <marcin.grzeszczak+github@gmail.com>
2020-12-13 20:10:45 +0100

    Added Go to list button from error page

--- src/app/Components/NotFoundPage/NotFound.component.ts
+++ src/app/Components/NotFoundPage/NotFound.component.ts
@@ -1,12 +1,18 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnDestroy, OnInit} from '@angular/core'
 import { BreakpointObserver } from '@angular/cdk/layout'
+import { Router } from '@angular/router'
+import {Subscription } from 'rxjs'
+import { PlanetsStoreService } from 'src/app/Services/PlanetsStore.service'
 
 @Component({
     selector: 'app-not-found',
     templateUrl: './NotFound.component.html',
     styleUrls: ['./NotFound.component.css']
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent implements OnInit, OnDestroy {
+    private observer767: Subscription
+    private observer480: Subscription
+
     imageContainerClasses = {
         'image_container': true,
         'image_container_480': false
@@ -18,10 +24,14 @@ export class NotFoundComponent implements OnInit {
         'error_text_480': false
     }
 
-    constructor(private breakPointObserver: BreakpointObserver) {}
+    constructor(private state: PlanetsStoreService, private router: Router,private breakPointObserver: BreakpointObserver) {}
+
+    goToList(): void {
+        this.state.planetNotFoundSub.next(false)
+      }
 
     private queries() {
-        this.breakPointObserver.observe(['(max-width: 767px)'])
+       this.observer767 = this.breakPointObserver.observe(['(max-width: 767px)'])
         .subscribe(breakState => {
             if(breakState.matches) {
                 this.errorTextClasses.error_text_767 = true
@@ -30,7 +40,7 @@ export class NotFoundComponent implements OnInit {
                 this.errorTextClasses.error_text_767 = false
         })
 
-        this.breakPointObserver.observe(['(max-width: 480px)'])
+        this.observer480 = this.breakPointObserver.observe(['(max-width: 480px)'])
         .subscribe(breakState => {
             if(breakState.matches) {
                 this.errorTextClasses.error_text_480 = true
@@ -49,4 +59,8 @@ export class NotFoundComponent implements OnInit {
        this.queries()
     }
     
+    ngOnDestroy() {
+        this.observer480.unsubscribe()
+        this.observer767.unsubscribe()
+    }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PlanetsStoreService } from './Services/PlanetsStore.service';
 import {Planet} from './DataSchemes.model'
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-    constructor(private router:Router, private store: PlanetsStoreService) {}
-    isDataLoaded = false
-    ngOnInit() {
-        this.store.getAllPlanets().subscribe(() => {
-          this.isDataLoaded = true
-        })
-    }
+export class AppComponent implements OnInit, OnDestroy{
+  private notFoundSubject: Subscription
+  private planetsLoading: Subscription
+  constructor(private router:Router, private store: PlanetsStoreService) {}
+  ngOnDestroy(): void {
+    this.notFoundSubject.unsubscribe()
+    this.planetsLoading.unsubscribe()
+  }
+  isDataLoaded = false
+  ngOnInit() {
+      this.notFoundSubject = this.store.planetNotFoundSub.subscribe(
+        value => {
+          if(value){
+            this.router.navigateByUrl('/notfound')
+          }
+          else {
+            this.router.navigate([''])
+          }
+        }
+      )
+      this.planetsLoading = this.store.getAllPlanets().subscribe(() => {
+        this.isDataLoaded = true
+      })
+  }
 }
