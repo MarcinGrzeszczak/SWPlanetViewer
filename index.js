da7812b79647e31a213379f7faab0b4d6f4e394c
Marcin Grzeszczak <marcin.grzeszczak+github@gmail.com>
2020-12-14 04:25:07 +0100

    Adding cors headers

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const app = express()
 
 const PORT = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers”, “Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+})
+
 app.use(express.static(path.join(__dirname,'/dist/','SWPlanetViewer')))
 app.use((req, res) => {
     res.sendFile(path.join(__dirname,'dist','SWPlanetViewer','index.html'))
