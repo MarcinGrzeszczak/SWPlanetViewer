66ddab04f7ec9d2dfe38d585cfc0daf05f9d568b
Marcin Grzeszczak <marcin.grzeszczak+github@gmail.com>
2020-12-14 04:31:34 +0100

    Trying cors dependency

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 
 const PORT = process.env.PORT || 3000;
-
+app.use(cors())
 app.use(express.static(path.join(__dirname,'/dist/','SWPlanetViewer')))
 app.use((req, res) => {
     res.sendFile(path.join(__dirname,'dist','SWPlanetViewer','index.html'))
